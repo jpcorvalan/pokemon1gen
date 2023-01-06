02dd8b82aad8d72686192840d9b2e24088d91199
Juan Pablo Corvalán <juanpablocorvalan98@gmail.com>
2023-01-06 14:40:19 -0300

    pokemon repository added

---
+++ src/main/java/com/service/pokemon1gen/repositories/PokemonRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.service.pokemon1gen.repositories;
+
+import com.service.pokemon1gen.models.Pokemon;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author juan9
+ */
+@Repository
+public interface PokemonRepository extends PagingAndSortingRepository<Pokemon, Long> {
+}
