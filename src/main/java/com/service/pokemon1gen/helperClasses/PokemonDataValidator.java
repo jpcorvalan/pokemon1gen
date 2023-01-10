12e8894c9d3cff985e356525b29fa778473ee6eb
Juan Pablo Corvalán <juanpablocorvalan98@gmail.com>
2023-01-10 01:56:28 -0300

    pokemon and types constants validators

---
+++ src/main/java/com/service/pokemon1gen/helperClasses/PokemonDataValidator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.service.pokemon1gen.helperClasses;
+
+/**
+ *
+ * @author juan9
+ */
+public final class PokemonDataValidator {
+    public static final String NAME = "You need to specify the Pokemon name";
+    public static final String WEIGHT = "Weight can't be 0 or negative";
+    public static final String TYPES = "You need to specify the pokemon Types";
+    public static final String POKEDEX = "You need to insert a pokemon description";
+}
---
+++ src/main/java/com/service/pokemon1gen/helperClasses/TypeDataValidator.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.service.pokemon1gen.helperClasses;
+
+/**
+ *
+ * @author juan9
+ */
+public final class TypeDataValidator {
+    public static final String NAME = "You need to specify a pokemon type name";
+    public static final String REGEXP_NAME = "Invalid name for type";
+}
