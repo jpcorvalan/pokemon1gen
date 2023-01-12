92d42942d89b62e369e00979be140893ac56f1de
Juan Pablo Corvalán <juanpablocorvalan98@gmail.com>
2023-01-12 15:46:04 -0300

    string helper added

---
+++ src/main/java/com/service/pokemon1gen/helperClasses/StringHelper.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.service.pokemon1gen.helperClasses;
+
+/**
+ *
+ * @author juan9
+ */
+public class StringHelper {
+
+    public static String upperFirstLetter(String string) {
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
+}
